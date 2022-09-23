7015da639299e127fdb465dbac6b3f665da97052
Lucas Henrique Messias Gonçalves 003 <lucashenriquemessiasgoncalves@gmail.com>
2022-09-23 10:06:59 -0300

    Create files for portfolio

---
+++ venv/portfolio.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Create the 1st page 
+@app.route("/") #Route -> caminho depois do domínio (meusite.com/rota/rotafilho)
+def homepage(): #Function -> o que será exibido na página
+    return render_template("homepage.html")
+
+@app.route("/about-me")
+def aboutMe():
+    return render_template("about_me.html")
+
+
+#Initializate the site
+if __name__ == "__main__":
+    app.run(debug=True)
